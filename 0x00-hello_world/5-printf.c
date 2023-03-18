79a36ee7361fcf5bf3f382102bb6a6b08fcc0a86
WasupOyin <kehindeonaolapo96@gmail.com>
2023-03-17 23:36:32 -0700

    dearies

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,12 +10,3 @@ int main(void)
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Cont
