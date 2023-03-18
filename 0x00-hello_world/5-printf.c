cdeb7be10ab6acb9b83c906e696154f312ff1001
WasupOyin <kehindeonaolapo96@gmail.com>
2023-03-17 23:31:27 -0700

    dearies

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print the strings in the printf function
+ * Description: prints out a string to stdout
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Cont
